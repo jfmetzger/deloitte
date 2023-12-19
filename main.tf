f7cd0fb090f8e9766b3af6eb1bda0f914e92b6a0
jfmetzger <jfmetzger65@gmail.com>
2023-12-19 08:40:21 -0600

    ran terraform fmt

--- main.tf
+++ main.tf
@@ -7,20 +7,20 @@ module "vpc" {
 }
 
 module "instance-group" {
-  source = "./modules/instance-group"
-  project = var.project
-  regions = var.regions
-  machine_type = var.machine_type
-  preemptible = var.preemptible
+  source         = "./modules/instance-group"
+  project        = var.project
+  regions        = var.regions
+  machine_type   = var.machine_type
+  preemptible    = var.preemptible
   instance_count = var.instance_count
   startup_script = var.startup_script
-  vpc_id = module.vpc.vpc_id
-  subnets = module.vpc.subnets
+  vpc_id         = module.vpc.vpc_id
+  subnets        = module.vpc.subnets
 }
 
 module "load-balancer" {
-  source = "./modules/load-balancer"
-  project = var.project
-  regions = var.regions
+  source         = "./modules/load-balancer"
+  project        = var.project
+  regions        = var.regions
   instance_group = module.instance-group.instance_group
 }
