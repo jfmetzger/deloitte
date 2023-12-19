16bed470d92c183cf5350f7c727d1f12d9ed9bfd
jfmetzger <jfmetzger65@gmail.com>
2023-12-19 14:36:07 -0600

    updated provider

--- provider.tf
+++ provider.tf
@@ -13,6 +13,6 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("../my-capstone-project-407419-a5e6ffb97061.json")
+#  credentials = file("../my-capstone-project-407419-a5e6ffb97061.json")
   project     = var.project_id
 }
